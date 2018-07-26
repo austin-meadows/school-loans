10ac74fb7e738551d8e447f7fee25836237e1f7f
Austin Meadows <austin@cheesy.pizza>
2018-07-25 20:46:03 -0400

    Section

---
+++ src/components/Section.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="section">
+    <div v-if="title" class="title">
+      {{ title }}
+    </div>
+    <div class="msg">
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Section',
+  props: {
+    msg: String,
+    title: String,
+  },
+};
+</script>
+
+<style scoped lang="stylus">
+.section {
+  height: 100vh;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Section
+      title="Student Loans are a problem"
+      msg="It is estimated the current total student loan debt is close to $2 trillion."
+    />
+    <Section
+      title="We are the solution"
+      msg="Together, we can eliminate this problem."
+    />
+    <Section
+      title="Post your student loan"
+      msg="Track your student loan. Get money to pay off your loan directly."
+    />
+    <Section
+     title="Or help out students like you"
+     msg="Help pay off other student's loans."
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Section from '@/components/Section.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    Section,
   },
 };
 </script>
