db29ccf74079682cdf9b10213642e7937311c15e
Austin Meadows <austin@cheesy.pizza>
2018-07-26 22:04:13 -0400

    some more styling for home page

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,13 @@
 </template>
 
 <style lang="stylus">
+@import '../node_modules/normalize-styl/normalize'
+
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   text-align center
-  color #2c3e50
 
 #nav
   padding 30px
--- src/components/Section.vue
+++ src/components/Section.vue
@@ -19,8 +19,28 @@ export default {
 };
 </script>
 
-<style scoped lang="stylus">
+<style lang="stylus">
+@import '../styles/palette.styl';
+@import '../styles/sizes.styl';
+
 .section {
+  align-items: center;
+  background-color: $color1;
+  display: flex;
+  flex-direction: column;
   height: 100vh;
+  justify-content: center;
 }
+
+.title {
+  font-size: $sizes-l;
+  font-style: italic;
+  margin-right: $sizes-xl;
+}
+
+.msg {
+  font-weight: lighter;
+  margin-left: $sizes-xl;
+}
+
 </style>
