a54b6c9902221983818d20361483a267ea86b25c
Austin Meadows <austin@cheesy.pizza>
2018-07-30 10:23:55 -0400

    put navigation in its own file

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,23 @@
 <script>
+import Navigation from '@/Navigation.vue';
+
 export default {
-  methods: {
-    onClickMenu() {
-      this.$refs.mobileMenu.classList.toggle('closed');
-    },
-    onClickMenuLink() {
-      this.$refs.mobileMenu.classList.add('closed');
-    },
+  components: {
+    Navigation,
   },
 };
 </script>
 
 <template>
   <div id="app">
-    <div id="nav" class="small">
-      <a @click="onClickMenu"><fa icon="bars" /> Menu</a>
-      <div ref="mobileMenu" class="mobileMenu closed">
-        <router-link v-on:click.native="onClickMenuLink" to="/">
-          <fa icon="home" /> Home
-        </router-link>
-        <router-link v-on:click.native="onClickMenuLink" to="/give">
-          <fa icon="piggy-bank" /> Give
-        </router-link>
-        <router-link v-on:click.native="onClickMenuLink" to="/stats">
-          <fa icon="chart-bar" /> Statistics
-        </router-link>
-        <router-link v-on:click.native="onClickMenuLink" to="/login">
-          <fa icon="sign-in-alt" /> Login
-        </router-link>
-      </div>
-    </div>
-    <div id="nav" class="large">
-      <router-link to="/"><fa icon="home" /> Home</router-link>
-      <router-link to="/give"><fa icon="piggy-bank" /> Give</router-link>
-      <router-link to="/stats"><fa icon="chart-bar" /> Statistics</router-link>
-      <router-link to="/login"><fa icon="sign-in-alt" /> Login</router-link>
-    </div>
+    <Navigation />
     <router-view/>
   </div>
 </template>
 
 <style lang="stylus">
 @import '../node_modules/normalize-styl/normalize';
-@import './styles/breakpoints.styl';
-@import './styles/palette.styl';
 @import './styles/sizes.styl';
-
-$screenSizeSmPlusOne = $screen-size-s + 1;
 
 body {
   font-size: $base;
@@ -55,55 +26,5 @@ body {
 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-}
-
-#nav {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  overflow: auto;
-
-  @media screen and (max-width: $screen-size-s) {
-    &.large {
-      display: none;
-    }
-  }
-
-  @media screen and (min-width: $screenSizeSmPlusOne) {
-    &.small {
-      display: none;
-    }
-  }
-
-  a {
-    color: #2c3e50;
-    cursor: pointer;
-    display: block;
-    line-height: $base;
-    padding: $base;
-    text-decoration: none;
-    white-space: nowrap;
-
-    &.router-link-exact-active {
-      color: $color1;
-    }
-
-    &:hover {
-      color: $color1;
-    }
-  }
-
-  .closed {
-    display: none;
-  }
-}
-
-.mobileMenu {
-  background: white;
-  margin-top: ($base * 3); // comes from line-height + padding-top + padding-bottom.
-  position: absolute;
-  text-align: right;
-  width: 100%;
-  z-index: 2;
 }
 </style>
