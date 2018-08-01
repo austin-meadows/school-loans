b9eb90cafa35a73f4d31cb7d59486a2141c0765b
Austin Meadows <austin@cheesy.pizza>
2018-07-31 22:51:03 -0400

    set up firebase db

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
+import VueFire from 'vuefire';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
 // font awesome icons
 import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
 import { faChartBar } from '@fortawesome/free-solid-svg-icons/faChartBar';
@@ -12,12 +14,20 @@ import { faSignInAlt } from '@fortawesome/free-solid-svg-icons/faSignInAlt';
 
 import App from './App.vue';
 import router from './router';
+import config from '../.firebase.config';
+
+
+Vue.use(VueFire);
 
 library.add(faBars, faChartBar, faHome, faPiggyBank, faSignInAlt);
 
 Vue.component('fa', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
+
+firebase.initializeApp(config);
+
+export default firebase.firestore();
 
 new Vue({
   router,
