5a13ed53416e6da6b6c65552a5a893a01e376c9e
Austin Meadows <yadon@Austins-MacBook-Pro.local>
2020-02-08 20:44:52 -0500

    update eslint, firestore stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/airbnb", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/airbnb", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
@@ -13,7 +13,7 @@ module.exports = {
   },
   overrides: [
     {
-      files: ["**/__tests__/*.{j,t}s?(x)"],
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
       env: {
         mocha: true
       }
